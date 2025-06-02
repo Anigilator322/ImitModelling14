67d118331e332e8f72bc7bafa6fd1e615f0638aa
Anigilator322 <Anigilator322@gmail.com>
2025-06-02 21:11:21 +0700

    layers

---
+++ ImitModelling.LoggerApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImitModelling.LoggerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
